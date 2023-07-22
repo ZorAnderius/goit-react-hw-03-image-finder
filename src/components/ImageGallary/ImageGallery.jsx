05cca2ba287c0e490a1bbfaeb8a674f953f578dd
ZorAnderius <gabriel.zorander@gmail.com>
2023-07-22 14:16:04 +0100

    Update ImageGallery.jsx

--- src/components/ImageGallary/ImageGallery.jsx
+++ src/components/ImageGallary/ImageGallery.jsx
@@ -1,6 +1,5 @@
 import { Component } from 'react';
 import React from 'react';
-import { toast } from 'react-toastify';
 import { axiosAPI } from 'api/pixabay_api';
 
 import imageGallaryCSS from './ImageGallery.module.css';
